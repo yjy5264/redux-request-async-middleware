fbce04c6185a4c61175af6e69a4a4363b36a9d54
余进洋 <yjy5264@sina.com>
2018-05-09 17:11:33 +0800

    update

--- dist/dispatch/dispatch.js
+++ dist/dispatch/dispatch.js
@@ -12,12 +12,15 @@ var _dispatch = void 0; /**
                          */
 var initReduxRequest = exports.initReduxRequest = function initReduxRequest(store) {
     _dispatch = store.dispatch;
+    return true;
 };
 
 var clear = exports.clear = function clear(subject) {
     _dispatch((0, _action.fetchPostsClear)(subject));
+    return true;
 };
 
 var request = exports.request = function request(subject, model, next) {
     _dispatch((0, _action.fetchPostsRequest)(subject, model, next));
+    return true;
 };
--- src/dispatch/dispatch.js
+++ src/dispatch/dispatch.js
@@ -10,12 +10,15 @@ let _dispatch;
 
 export const initReduxRequest = store => {
     _dispatch = store.dispatch;
+    return true;
 };
 
 export const clear = subject => {
     _dispatch(fetchPostsClear(subject));
+    return true;
 };
 
 export const request = (subject, model, next) => {
     _dispatch(fetchPostsRequest(subject, model, next));
+    return true;
 };
